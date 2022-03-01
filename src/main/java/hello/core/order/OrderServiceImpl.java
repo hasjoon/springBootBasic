1c104f80acfac4d547324d3d0a36a0011cecbd44
Sungjoon Ha <osn6629@naver.com>
2022-03-02 03:46:55 +0900

    생성자 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,11 +9,19 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy; //dip를 지킴 근데 구체객체가 없어서 안돌아감
+//    private DiscountPolicy discountPolicy; //dip를 지킴 근데 구체객체가 없어서 안돌아감
+    private final DiscountPolicy discountPolicy; //파이널 있으면 기본할당 or 생성자 할당 필요
 
+    public OrderServiceImpl(MemberRepository memberRepository,
+        DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {

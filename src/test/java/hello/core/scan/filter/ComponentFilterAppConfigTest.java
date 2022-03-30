e3420373a8207801bad89be4c9d12c85446b1260
Sungjoon Ha <osn6629@naver.com>
2022-03-30 10:38:12 +0900

    add fix code for "conflictingBeanDeefinitionException" in properties

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -35,6 +35,6 @@ public class ComponentFilterAppConfigTest {
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
     )
     static class ComponentFilterAppConfig{
-
+        
     }
 }
